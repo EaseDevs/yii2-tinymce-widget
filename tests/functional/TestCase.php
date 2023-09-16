b0d0ad79c8e836e63cdb2bf2fdf7a473f91966b3
Steven Michaels <steven.michaels@gmail.com>
2023-09-16 16:44:49 -0500

    Updating to easedevs namespace, remove unused CI

--- tests/functional/TestCase.php
+++ tests/functional/TestCase.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @link https://github.com/2amigos/yii2-tinymce-widget
+ * @link https://github.com/easedevs/yii2-tinymce-widget
  * @copyright Copyright (c) 2013-2015 2amigOS! Consulting Group LLC
  * @license http://opensource.org/licenses/BSD-3-Clause
  */
